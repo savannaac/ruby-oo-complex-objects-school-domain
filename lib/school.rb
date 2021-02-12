226031e24bc1fb10fa5c312aaf8332ad7e84a8db
savanna chiang <savannaac@gmail.com>
2021-02-11 21:23:49 -0600

    School Domain-final

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+class School
+    attr_accessor :roster, :name
+
+    def initialize(name)
+        @roster = {}
+        @name = name
+    end
+
+    def add_student(name, grade)
+        if @roster[grade]
+            @roster[grade] << name
+        else
+            @roster[grade] = []
+            @roster[grade] << name
+        end
+    end
+
+    def grade(grade)
+        roster[grade]
+    end
+
+    def sort
+        roster.each { |k, v| v.sort! }.sort.to_h
+    end
+
+end
